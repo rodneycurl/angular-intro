a28eac1f371467776cd00211fcdb997de49e0ed1
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-03-06 10:15:54 -0500

    angular project

---
+++ angularTodos/app.js
@@ -0,0 +1,12 @@
+// app.js
+let todoData = [
+  { name: 'Build an app with Rails', completed: true },
+  { name: 'Project 2', completed: true },
+  { name: 'Build an app with Angular', completed: false },
+  { name: 'Project 3', completed: false },
+  { name: 'Build an app with Express', completed: false },
+  { name: 'Build an app with Mongo', completed: false },
+  { name: 'Build an app with React', completed: false },
+  { name: 'Project 4', completed: false },
+  { name: 'Become a Rockstar', completed: true }
+]
